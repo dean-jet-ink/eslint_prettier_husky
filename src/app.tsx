4300611b5e20bd62f938b9eab84dd4ecae2a4b0f
unknown <rr7i5fvv@gmail.com>
2024-02-07 14:04:48 +0900

    commit

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,10 @@
-type appProps = {
+type AppProps = {
 	message: string;
 };
 
-export const App = ({ message }: appProps) => {
+export const App = ({ message }: AppProps) => {
+	console.log(message);
+
 	if (true) {
 		return <div>{message}</div>;
 	}
